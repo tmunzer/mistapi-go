b9c20a305b8cefcb65bb9fe6825e808d7ea90a88
Thomas Munzer <tmunzer@juniper.net>
2024-07-05 10:37:26 +0200

    0.1.12

--- sdk/configuration.go
+++ sdk/configuration.go
@@ -93,7 +93,7 @@ type Configuration struct {
 func NewConfiguration() *Configuration {
 	cfg := &Configuration{
 		DefaultHeader:    make(map[string]string),
-		UserAgent:        "OpenAPI-Generator/0.1.11/go",
+		UserAgent:        "OpenAPI-Generator/0.1.12/go",
 		Debug:            false,
 		Servers:          ServerConfigurations{
 			{
